0356c40e2f759f1dcab8c3f4c003efb980cb0298
alexhateful <alexhateful@gmail.com>
2018-11-29 20:04:33 +0200

    update arrays

---
+++ Arrays/js/scripts variables.js
@@ -0,0 +1,14 @@
+let first = 'first',
+    second = 'second',
+    third = 'third';
+    
+const message = 'hello, ';
+
+console.log(message + first);
+
+first = 'FIRST';
+
+console.log(message + first);
+
+console.log(message + second);
+console.log(message + third);
---
+++ Arrays/js/scripts.js
@@ -0,0 +1,16 @@
+let index = 0,
+message = '';
+
+for (let i = 0; i <= 5, j < n, k < n; i++) {
+
+    for (let j = 0; j < index; j++) {
+        
+        message = message + index + ' ';
+        
+    }
+
+    console.log(message);
+    index++;
+    message = '';
+
+}
